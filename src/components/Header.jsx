a11d15061e4d6a3898a34aa5b0a89f428e42947d
metalBread33 <kfoughty80@gmail.com>
2024-09-09 08:19:49 -0400

    Add Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Navbar, Nav} from 'react-bootstrap'
-import {FaFileCode} from 'react-icons/fa'
+import {FaFileCode, FaPhone, FaBriefcase} from 'react-icons/fa'
 
 const Header = () => {
   return (
     <>
         <header>
-            <Navbar expand='lg' bg='dark' variant='dark'> 
+            <Navbar expand='lg' bg='light' variant='light' > 
+            <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                 <Navbar.Brand>Kevin Foughty's portfolio</Navbar.Brand>
-                <Nav.Link><FaFileCode/></Nav.Link>
+                <Nav className='ms-auto'>
+                  <Nav.Link> <FaPhone/> Contact</Nav.Link>
+                  <Nav.Link> <FaBriefcase/> Work</Nav.Link>
+                  <Nav.Link><FaFileCode/>Projects</Nav.Link>
+                </Nav>
             </Navbar>
         </header>
     </>
